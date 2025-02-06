fc48545cf0812bc55f7c239241d68771b621ce17
John <jbotonakis@carroll.edu>
2025-02-06 16:53:28 -0700

    Created new branch for database work

--- src/main/java/com/retriage/retriage/RetriageApplication.java
+++ src/main/java/com/retriage/retriage/RetriageApplication.java
@@ -2,7 +2,7 @@ package com.retriage.retriage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Database Branch! Not Merged Yet!
 @SpringBootApplication
 public class RetriageApplication {
 
