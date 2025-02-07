9eae34d438e892af690ac658b0bd866a5d086a61
John <jbotonakis@carroll.edu>
2025-02-06 17:06:36 -0700

    Updated Dependencies in build.gradle.kts

---
+++ src/main/java/com/retriage/retriage/domain/Contact.java
@@ -0,0 +1,15 @@
+package com.retriage.retriage.domain;
+
+
+@Entity
+public class Contact {
+    private String ID;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String condition;
+    private String photoID;
+    public Contact(String ID, String firstName, String lastName, String email, String phone) {
+
+    }
+}
