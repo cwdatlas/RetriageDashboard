13296b35efb7f74e871e9370b27a716dab16ea86
John <jbotonakis@carroll.edu>
2025-02-06 17:13:16 -0700

    Setup "Contact" as done in tutorial; Imported jakarta, and lombok; added to dependencies

--- src/main/java/com/retriage/retriage/domain/Contact.java
+++ src/main/java/com/retriage/retriage/domain/Contact.java
@@ -1,8 +1,29 @@
 package com.retriage.retriage.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
 
 @Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_DEFAULT)
 public class Contact {
+    @Id
+    @UuidGenerator
+    @Column(name = "ID", unique = true,updatable = false)
     private String ID;
     private String firstName;
     private String lastName;
