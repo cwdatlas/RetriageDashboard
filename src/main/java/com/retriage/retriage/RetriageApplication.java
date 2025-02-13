2c8c24a0d5fd34f216a0fe71b2a79bd2ec824aa2
cwdatlas <ascott@carroll.edu>
2025-02-13 11:59:10 -0700

    refactor

--- src/main/java/com/retriage/retriage/RetriageApplication.java
+++ src/main/java/com/retriage/retriage/RetriageApplication.java
@@ -2,12 +2,11 @@ package com.retriage.retriage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Database Branch! Not Merged Yet!
+
 @SpringBootApplication
 public class RetriageApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(RetriageApplication.class, args);
 	}
 
