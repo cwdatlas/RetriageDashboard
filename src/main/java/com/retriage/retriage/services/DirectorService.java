b91101e8404d706a2dd5258a77b1963187e40b5b
John <jbotonakis@carroll.edu>
2025-02-17 20:29:48 -0700

    Created Director and DirectorService on main because I was not looking where I was coding. Will make new branch. Whoops.

---
+++ src/main/java/com/retriage/retriage/services/DirectorService.java
@@ -0,0 +1,11 @@
+package com.retriage.retriage.services;
+
+import com.retriage.retriage.models.Patient;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DirectorService {
+
+}
