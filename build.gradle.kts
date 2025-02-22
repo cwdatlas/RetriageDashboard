449215938dc99a5ba609bf851374e59674bc9d90
John <jbotonakis@carroll.edu>
2025-02-22 14:25:29 -0700

    OKTA NOW CONNECTED LFG!; deleted okta items in application.properties;renamed app.yaml to app.yml;fleshed out application.yml;cleaned up repositories and dependencies in build.gradle.kts;added a temp home html and controller for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,27 +15,40 @@ java {
 
 repositories {
 	mavenCentral()
+//	maven { url = uri("https://repo.maven.apache.org/maven2/") } // Keep explicit Maven Central URL
+	jcenter() // ADD jcenter() repository - for broader search, as a fallback
+	maven { url = uri("https://build.shibboleth.net/nexus/content/repositories/releases/") } // Add Shibboleth repo
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	//Okta Dependencies below:
 	implementation("org.springframework.boot:spring-boot-starter-security");
-//	implementation("org.springframework.security:spring-security-saml2-service-provider") // SAML2
+	implementation("org.springframework.boot:spring-boot-starter-web");
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6");
+	implementation("org.springframework.boot:spring-boot-starter-test");
+	implementation("org.springframework.security:spring-security-test");
+//	implementation("org.springframework.security:spring-security-saml2-service-provider")
+//	implementation("org.opensaml:opensaml-core:4.3.0")
+//	implementation("org.opensaml:opensaml-saml-api:4.3.0")
+//	implementation("org.opensaml:opensaml-saml-impl:4.3.0")
+
+	constraints {
+		implementation ("org.opensaml:opensaml-core:4.1.1")
+		implementation ("org.opensaml:opensaml-saml-api:4.1.1")
+		implementation ("org.opensaml:opensaml-saml-impl:4.1.1")
+	}
+	implementation ("org.springframework.security:spring-security-saml2-service-provider")
 
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-
-//Needed for forcing dependency in Okta
-configurations.all {
-	resolutionStrategy.force("org.opensaml:opensaml-saml-impl:4.0.1")// Or try 4.1.1 if 4.0.1 doesn't work later
 }
---
+++ src/main/java/com/retriage/retriage/HomeController.java
@@ -0,0 +1,21 @@
+package com.retriage.retriage;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    AuthenticationPrincipal Saml2AuthenticatedPrincipal;
+
+    @RequestMapping("/")
+    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+        model.addAttribute("name", principal.getName());
+        model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
+        model.addAttribute("userAttributes", principal.getAttributes());
+        return "home";
+    }
+
+}
