4043614b766d097e40677f11ae6b90c6397b3800
John <jbotonakis@carroll.edu>
2025-02-22 13:29:50 -0700

    Got to a point where it runs now; Added saml2 config in application.properties;commented out problematic lines;deleted previous security config

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,11 +26,9 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	//Okta Dependencies below:
-	implementation("org.springframework.boot:spring-boot-starter-security") //Security Springboot stuffs
-	implementation("org.springframework.security:spring-security-saml2-service-provider:6.0.6") // SAML2
-	implementation("org.opensaml:opensaml-core:4.0.1")    // Explicitly set OpenSAML core to 4.0.1
-	implementation("org.opensaml:opensaml-saml-api:4.0.1")  // Explicitly set OpenSAML api to 4.0.1
-//	implementation("org.opensaml:opensaml-saml-impl:4.1.1")
+	implementation("org.springframework.boot:spring-boot-starter-security");
+//	implementation("org.springframework.security:spring-security-saml2-service-provider") // SAML2
+
 }
 
 tasks.withType<Test> {
