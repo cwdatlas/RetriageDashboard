39762566d2b087415e2408d059b353c28f73b99d
John <jbotonakis@carroll.edu>
2025-02-26 12:33:00 -0700

    Changed User and UserForm to better ignore Lombok getters and setters for testing.

--- src/main/java/com/retriage/retriage/forms/UserForm.java
+++ src/main/java/com/retriage/retriage/forms/UserForm.java
@@ -24,6 +24,46 @@ public class UserForm {
     @Setter
     private List<Patient> createdPatients;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Patient> getCreatedPatients() {
+        return createdPatients;
+    }
+
+    public void setCreatedPatients(List<Patient> createdPatients) {
+        this.createdPatients = createdPatients;
+    }
+
     public UserForm() {
     }
 }
