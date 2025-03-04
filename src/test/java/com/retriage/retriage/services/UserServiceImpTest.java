1dfa7db86ac356fbf3cfc7e3fccbe981586aa57c
John <jbotonakis@carroll.edu>
2025-03-04 13:06:00 -0700

    Created UserServiceImpTest, deleted models in test pacakge;added logging to properties

---
+++ src/test/java/com/retriage/retriage/services/UserServiceImpTest.java
@@ -0,0 +1,11 @@
+package com.retriage.retriage.services;
+//1) Add logging to this; Done after or inside of the method
+    //[Status] Method name: Error Message
+    //Log statuses: Warning, Severe, Error, etc
+//2) Build a fake user setup
+//3) Throw errors
+
+
+public class UserServiceImpTest {
+
+}
