04985e278078fed45b2ab4e8fb9f9f29d8c44756
John <jbotonakis@carroll.edu>
2025-03-11 11:33:23 -0600

    Added constraints to methods in EventForm

--- src/main/java/com/retriage/retriage/forms/EventForm.java
+++ src/main/java/com/retriage/retriage/forms/EventForm.java
@@ -3,6 +3,8 @@ package com.retriage.retriage.forms;
 import com.retriage.retriage.enums.Status;
 import com.retriage.retriage.models.Resource;
 import com.retriage.retriage.models.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,25 +15,46 @@ import java.util.List;
 public class EventForm {
     @Getter
     @Setter
+    @NotBlank(message = "Event name is required")
     private String name;
+
     @Getter
     @Setter
+    @NotBlank(message = "Director is required")
     private User director;
+
     @Getter
     @Setter
-    private int startTime;
-    @Getter
-    @Setter
-    private int endTime;
-    @Getter
-    @Setter
-    private Status status;
-    @Getter
-    @Setter
-    private List<Resource> resources;
-    @Getter
-    @Setter
+    @NotNull(message = "Must contain at least one nurse")
     private List<User> nurses;
+
+    @Getter
+    @Setter
+    @NotNull(message = "Must contain at least one Resource")
+    private List<Resource> resources;
+
+    @Getter
+    @Setter
+    @NotBlank(message = "Status is required")
+    private Status status;
+
+//    @NotNull(message = "Start time is required")
+//    @FutureOrPresent(message = "Start time must be in the present or future")
+//    private LocalDateTime startTime;
+//
+//    @NotNull(message = "End time is required")
+//    @FutureOrPresent(message = "End time must be in the present or future")
+//    private LocalDateTime endTime;
+
+    @Getter
+    @Setter
+    @NotNull(message = "Start time is required")
+    private int startTime;
+
+    @Getter
+    @Setter
+    @NotNull(message = "End time is required")
+    private int endTime;
 
     public EventForm() {
     }
