28c003b8d24a69ecfe50eb7a8440dfe544e79328
John <jbotonakis@carroll.edu>
2025-03-11 10:53:16 -0600

    Added getters and setters to EventForm

--- src/main/java/com/retriage/retriage/forms/EventForm.java
+++ src/main/java/com/retriage/retriage/forms/EventForm.java
@@ -4,18 +4,33 @@ import com.retriage.retriage.enums.Status;
 import com.retriage.retriage.models.Resource;
 import com.retriage.retriage.models.User;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Data
 public class EventForm {
-
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private User director;
+    @Getter
+    @Setter
     private int startTime;
+    @Getter
+    @Setter
     private int endTime;
+    @Getter
+    @Setter
     private Status status;
+    @Getter
+    @Setter
     private List<Resource> resources;
+    @Getter
+    @Setter
     private List<User> nurses;
 
     public EventForm() {
