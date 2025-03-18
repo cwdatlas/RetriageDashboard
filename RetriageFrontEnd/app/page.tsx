8c126a07e3f45b4f991a67e0557ad960f69d230f
cwdatlas <ascott@carroll.edu>
2025-03-18 09:11:07 -0600

    cut unused imports

--- RetriageFrontEnd/app/page.tsx
+++ RetriageFrontEnd/app/page.tsx
@@ -2,8 +2,6 @@
 
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
-import {SetCookies} from "@/app/api/cookieApi";
-import {Role} from "@/app/enumerations/role"
 
 export default function Home() {
     return (
