7597635a96aefbcd4099d45dccaa9968b55ade25
cwdatlas <ascott@carroll.edu>
2025-03-18 14:51:49 -0600

    swapped @getter and @setters for @Data. Added pooltype attribute

--- src/main/java/com/retriage/retriage/forms/PatientPoolForm.java
+++ src/main/java/com/retriage/retriage/forms/PatientPoolForm.java
@@ -1,32 +1,31 @@
 package com.retriage.retriage.forms;
 
+import com.retriage.retriage.enums.PoolType;
 import com.retriage.retriage.models.Patient;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
 
 import java.util.List;
 
-public class ResourceForm {
-    @Getter
-    @Setter
+@Data
+public class PatientPoolForm {
+
     @NotBlank(message = "PatientPool name is required")
     private String name;
-    @Getter
-    @Setter
+
     @Positive(message = "Process time must be a positive number")
     private int processTime;
-    @Getter
-    @Setter
+
     private boolean active;
-    @Getter
-    @Setter
+
     private boolean useable;
-    @Getter
-    @Setter
+
     private List<Patient> patients;
 
-    public ResourceForm() {
+    private PoolType poolType;
+
+    public PatientPoolForm() {
     }
 }
