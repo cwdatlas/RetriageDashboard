de87611b64cb32e36788d7481302c4683eeec60f
cwdatlas <ascott@carroll.edu>
2025-03-20 14:04:23 -0600

    started on Event visualization

---
+++ RetriageFrontEnd/app/components/eventPanel.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function EventPanel({activeEvent : {activeEvent : Event}}){
+    return (
+        <main>
+            <div>Active event details</div>
+        </main>
+    )
+}
