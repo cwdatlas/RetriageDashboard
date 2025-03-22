fc442a4a78b1c05df5bb9cd5fe48b1517b14d21a
John <jbotonakis@carroll.edu>
2025-03-22 12:11:30 -0600

    Created an Error Response handler;

---
+++ src/main/java/com/retriage/retriage/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.retriage.retriage.exceptions; // Or a suitable package
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path; // Optional: The request path that caused the error
+}
