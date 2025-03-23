e228c70d4f494f1fa7510b1e459b5e8312f0ebde
John <jbotonakis@carroll.edu>
2025-03-23 14:26:58 -0600

    Adjusted the way logouts are handled in the Security Config

--- src/main/java/com/retriage/retriage/SecurityConfiguration.java
+++ src/main/java/com/retriage/retriage/SecurityConfiguration.java
@@ -61,14 +61,20 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/api/pools/templates").permitAll()
-                        // Allow POST requests to /uploadImage for authenticated users
-                        .requestMatchers(org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/uploadImage")).authenticated()
-                        .anyRequest().authenticated()) // Require authentication for any request to this application
+                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/api/pools/templates").permitAll()
+                // Allow POST requests to /uploadImage for authenticated users
+                .requestMatchers(org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/uploadImage")).authenticated()
+                .anyRequest().authenticated()) // Require authentication for any request to this application
                 .saml2Login(saml2 -> saml2
-                        .authenticationManager(new ProviderManager(authenticationProvider))) // Use the custom SAML authentication provider
-                .saml2Logout(withDefaults()); // Enable SAML Logout with default configurations
+                .authenticationManager(new ProviderManager(authenticationProvider))) // Use the custom SAML authentication provider
+                .saml2Logout(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // This is where Spring Security listens for logout requests
+                        .logoutSuccessUrl("/") // Redirect after logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")); // Enable SAML Logout with default configurations
 
         return http.build(); // Build and return the configured SecurityFilterChain
     }
