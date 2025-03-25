0f41c3d5939e024ccea2e1d2c498c4a90b7299a6
cwdatlas <ascott@carroll.edu>
2025-03-25 10:30:37 -0600

    added permission for websocket endpoints

--- src/main/java/com/retriage/retriage/configurations/SecurityConfiguration.java
+++ src/main/java/com/retriage/retriage/configurations/SecurityConfiguration.java
@@ -65,6 +65,9 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
+                        .requestMatchers("/topic/**").permitAll()
+                        .requestMatchers("/active_event/**").permitAll()
                         .anyRequest().authenticated()) // Require authentication for any request to this application
                 .saml2Login(saml2 -> saml2
                 .authenticationManager(new ProviderManager(authenticationProvider)) // Use the custom SAML authentication provider
