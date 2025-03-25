247056e013f9995fdcffb9075d85801e57eea19a
cwdatlas <ascott@carroll.edu>
2025-03-24 21:37:22 -0600

    added additional method of findByStatus

--- src/main/java/com/retriage/retriage/repositories/EventRepo.java
+++ src/main/java/com/retriage/retriage/repositories/EventRepo.java
@@ -1,8 +1,11 @@
 package com.retriage.retriage.repositories;
 
+import com.retriage.retriage.enums.Status;
 import com.retriage.retriage.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  *
@@ -10,5 +13,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
     //This is empty, intentionally
+    List<Event> findByStatus(Status status);
 
 }
