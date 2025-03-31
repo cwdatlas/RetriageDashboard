984398e7b97dcb5ce8015ccb1318d6b86437c3f4
cwdatlas <ascott@carroll.edu>
2025-03-31 10:14:51 -0600

    Added styling

--- RetriageFrontEnd/app/components/panel/bayPannel.tsx
+++ RetriageFrontEnd/app/components/panel/bayPannel.tsx
@@ -3,8 +3,9 @@
 import {PatientPool} from "@/app/models/patientPool";
 import PatientIcon from "@/app/components/panel/patientIcon";
 import {useDroppable} from "@dnd-kit/core";
+import {Event} from "@/app/models/event";
 
-export default function BayPanel({bay}: { bay: PatientPool }) {
+export default function BayPanel({bay, getActiveEvent}: { bay: PatientPool, getActiveEvent: () => Event }) {
     const {isOver, setNodeRef} = useDroppable({
         id: bay.id || 0,
     });
@@ -12,16 +13,18 @@ export default function BayPanel({bay}: { bay: PatientPool }) {
         color: isOver ? 'green' : undefined,
     };
     return (
-        <div ref={setNodeRef} style={style}>
-            <h2>{bay.name}</h2>
-            <div>
-                {bay.patients.map((template, idx) => {
-                    return (
-                        <div key={idx}>
-                            <PatientIcon patient={template}/>
+        <div ref={setNodeRef} style={style} className="card rounded mb-3">
+            <div className="card-header">
+                <h2 className="card-title">{bay.name}</h2>
+            </div>
+            <div className="card-body">
+                <div className="row row-cols-6">
+                    {bay.patients.map((patient, idx) => (
+                        <div key={idx} className="col mb-2">
+                            <PatientIcon patient={patient} patientList={bay.patients} getActiveEvent={getActiveEvent}/>
                         </div>
-                    )
-                })}
+                    ))}
+                </div>
             </div>
         </div>
     )
--- RetriageFrontEnd/app/components/panel/medServicePanel.tsx
+++ RetriageFrontEnd/app/components/panel/medServicePanel.tsx
@@ -3,26 +3,34 @@
 import PatientIcon from "@/app/components/panel/patientIcon";
 import {PatientPool} from "@/app/models/patientPool";
 import {useDroppable} from "@dnd-kit/core";
+import {Event} from "@/app/models/event";
 
-export default function MedServicePanel({service}: { service: PatientPool }) {
+export default function MedServicePanel({service, getActiveEvent}: {
+    service: PatientPool,
+    getActiveEvent: () => Event
+}) {
     const {isOver, setNodeRef} = useDroppable({
         id: service.id || 0,
     });
     const style = {
         color: isOver ? 'green' : undefined,
     };
+
     return (
-        <div ref={setNodeRef} style={style}>
-            <h2>{service.name}</h2>
-            <div>
-                {service.patients.map((template, idx) => {
-                    return (
-                        <div key={idx}>
-                            <PatientIcon patient={template}/>
+        <div ref={setNodeRef} style={style} className="card rounded mb-3">
+            <div className="card-header">
+                <h2 className="card-title">{service.name}</h2>
+            </div>
+            <div className="card-body">
+                <div className="row row-cols-6">
+                    {service.patients.map((patient, idx) => (
+                        <div key={idx} className="col mb-2">
+                            <PatientIcon patient={patient} patientList={service.patients}
+                                         getActiveEvent={getActiveEvent}/>
                         </div>
-                    )
-                })}
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
