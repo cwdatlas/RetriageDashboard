15bf895a69420c70bed8b037f807668f081b4aec
cwdatlas <ascott@carroll.edu>
2025-04-07 08:29:25 -0600

    added Created enumeration

--- RetriageFrontEnd/app/enumerations/status.tsx
+++ RetriageFrontEnd/app/enumerations/status.tsx
@@ -2,4 +2,5 @@ export enum Status {
     Running = "Running",
     Paused = "Paused",
     Ended = "Ended",
+    Created = "Created",
 }
--- src/main/java/com/retriage/retriage/enums/Status.java
+++ src/main/java/com/retriage/retriage/enums/Status.java
@@ -3,5 +3,6 @@ package com.retriage.retriage.enums;
 public enum Status {
     Running,
     Ended,
-    Paused
+    Paused,
+    Created,
 }
