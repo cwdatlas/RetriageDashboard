16750a1b41cac21ddcb5692452a404b5c259ff4b
cwdatlas <ascott@carroll.edu>
2025-04-10 07:42:39 -0600

    added method to grab users details based on jwt token

--- RetriageFrontEnd/app/api/userApi.tsx
+++ RetriageFrontEnd/app/api/userApi.tsx
@@ -1,5 +1,8 @@
-export const dynamic = 'force-static'
+import {UserDto} from "@/app/models/userDto";
 import {User} from "./../models/user";
+import {GetCookies} from "@/app/api/cookieApi";
+
+export const dynamic = 'force-static'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
 const endpoint = "/api/users"
@@ -41,17 +44,27 @@ export async function createUser(user: Omit<User, "id">): Promise<User> {
 /**
  * Optionally, get a single user by ID
  */
-export async function getUserById(id: number): Promise<User> {
-    const res = await fetch(`${API_BASE_URL}` + endpoint + `${id}`, {
+export async function getUserByToken(setUser: (user: UserDto) => void, setError: (error: string) => void): Promise<void> {
+    const jwt = GetCookies('JWT');
+    const res = await fetch(`${API_BASE_URL}` + endpoint + "/me", {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
+            "Authorization": jwt
         },
     });
-    if (!res.ok) {
-        throw new Error(`Failed to fetch user with ID ${id}`);
+    if (res.ok) {
+        console.log("User credentials received from backend.")
+        const user: UserDto = await res.json();
+        console.log(user)
+        setUser(user)
+    } else if (res.status == 401) {
+        console.log("User token invalid.")
+        setError("User token invalid.")
+    } else {
+        console.error("Failed to fetch user credentials");
+        setError("Failed to fetch user credentials")
     }
-    return res.json();
 }
 
 /**
