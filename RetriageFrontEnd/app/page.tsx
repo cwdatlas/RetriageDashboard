04f660044eed271d3844cf561f68d33851713378
John <jbotonakis@carroll.edu>
2025-04-12 17:42:33 -0600

    Fetch and display user info via token cookie!

--- RetriageFrontEnd/app/page.tsx
+++ RetriageFrontEnd/app/page.tsx
@@ -2,8 +2,18 @@
 
 import Header from "@/app/components/panel/header";
 import Footer from "@/app/components/panel/footer";
+import { useEffect, useState } from "react";
+import { getUserByToken } from "@/app/api/userApi";
+import { UserDto } from "@/app/models/userDto";
 
 export default function Home() {
+    const [user, setUser] = useState<UserDto | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        console.log("Calling getUserByToken...");
+       void getUserByToken(setUser, setError); // It returns a promise. I do not care.
+    }, []);
     return (
         <main className="d-flex flex-column min-vh-100">
             <Header/>
@@ -12,6 +22,14 @@ export default function Home() {
                 This website is to manage the data of the event and help nursing students get a better idea of what it
                 means
                 to work in a stressful and fast passed environment that is mimicking real nursing practices.</p>
+
+            {user && (
+                <p>Welcome, {user.username}! Your role is: {user.role}</p>
+            )}
+            {error && (
+                <p style={{ color: "red" }}>{error}</p>
+            )}
+
 
             <Footer/>
         </main>
