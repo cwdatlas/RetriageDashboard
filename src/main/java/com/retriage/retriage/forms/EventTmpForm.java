a6e4214737a227d367b754c57c9ef5de162f9d68
John <jbotonakis@carroll.edu>
2025-04-21 16:37:07 -0600

    Added proper javadoc commenting, and formatted each file. Directory "forms" fully completed

--- src/main/java/com/retriage/retriage/forms/EventTmpForm.java
+++ src/main/java/com/retriage/retriage/forms/EventTmpForm.java
@@ -8,20 +8,37 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Form object used for receiving data to create a new {@link com.retriage.retriage.models.Event}.
+ * This form is specifically designed for creating an event based on a list of {@link PatientPoolTmp}s (patient pool templates).
+ * Includes validation constraints to ensure data integrity upon submission.
+ */
 @Data
 public class EventTmpForm {
 
+    /**
+     * The name of the event to be created. Must not be blank.
+     */
     @NotBlank(message = "Event name is required")
     private String name;
 
+    /**
+     * The planned total duration of the event in milliseconds. Must not be null.
+     */
     @NotNull(message = "End time is required")
     private Long duration;
 
-    @NotNull(message = "Must contain at least one PatientPool")
-    @Size(min = 1, message = "Must contain at least one PatientPool")
+    /**
+     * A list of patient pool templates to use when creating the event's pools.
+     * Must not be null and must contain at least one template.
+     */
+    @NotNull(message = "Must contain at least one PatientPool Template")
+    @Size(min = 1, message = "Must contain at least one PatientPool Template")
     private List<PatientPoolTmp> poolTmps;
 
+    /**
+     * Default no-argument constructor.
+     */
     public EventTmpForm() {
     }
-
 }
--- src/main/java/com/retriage/retriage/forms/PatientForm.java
+++ src/main/java/com/retriage/retriage/forms/PatientForm.java
@@ -5,21 +5,42 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+/**
+ * Form object used for receiving patient data from clients, typically for creating or updating a patient.
+ * Includes validation constraints to ensure data integrity upon submission.
+ */
 @Data
 public class PatientForm {
 
+    /**
+     * The unique identifier of the patient. May be null for new patients.
+     */
     private Long id;
 
+    /**
+     * The patient's card ID or identifier. Must not be blank.
+     */
     @NotBlank(message = "Card ID is required")
     private String cardId;
 
+    /**
+     * The patient's medical condition or triage level. Must not be null.
+     * See {@link Condition} for possible values.
+     */
     @NotNull(message = "Condition is required")
     private Condition condition;
 
+    /**
+     * Flag indicating whether the patient is currently being processed. Must not be null.
+     * Note: {@code @NotBlank} is not applicable to boolean primitives; {@code @NotNull} is sufficient for validation.
+     */
     @NotNull(message = "BeingProcessed must be either true or false")
     @NotBlank(message = "BeingProcessed must be either true or false")
     private boolean beingProcessed;
 
+    /**
+     * Default no-argument constructor.
+     */
     public PatientForm() {
     }
 }
